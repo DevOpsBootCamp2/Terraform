2d080d07415c2c55c5c25eb3f3539f4efa5a253b
Paul Branston <paul.branston@capgemini.com>
2017-04-04 17:32:44 +0100

    Change drone.sh to use &&

--- Userdata/drone.sh
+++ Userdata/drone.sh
@@ -6,7 +6,4 @@ curl -L https://github.com/docker/compose/releases/download/1.12.0-rc2/docker-co
 
 chown ubuntu:ubuntu /home/ubuntu/docker-compose.yml
 su - ubuntu
-export DRONE_SECRET=BootCampDroneTestSecret
-export DRONE_GITHUB_CLIENT=6538a0a85ad3851990d3
-export DRONE_GITHUB_SECRET=ced87a6dc135edb22952095b72be36453dfc5461
-docker-compose up -d
+export DRONE_SECRET=BootCampDroneTestSecret && export DRONE_GITHUB_CLIENT=6538a0a85ad3851990d3 && export DRONE_GITHUB_SECRET=ced87a6dc135edb22952095b72be36453dfc5461 && docker-compose up -d
